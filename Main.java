5acf9b1dceebb308f5f859160a6db68d4a4eec0e
BigPoppaCode25 <p4k65wvbrm@privaterelay.appleid.com>
2026-09-18 23:42:41 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-public enum PatientCategory {
-    INPATIENT,
-    OUTPATIENT,
-    EMERGENCY
+public class Main {
+    public static void main(String[] args) {
+        Hospital hospital = new Hospital();
+        hospital. addPatient(new Patient( name "John", age 25 PatientCategory. GENERAL));
+        hospital.addPatient(new Patient(name "Sarah", age 70, PatientCategory. ELDERLY));
+        hospital. addPatient(new Patient(name "Baby Joe", age 2 PatientCategory,PEDIATRIC));
+        hospital.displayPatient();
+    }
 }
+        
